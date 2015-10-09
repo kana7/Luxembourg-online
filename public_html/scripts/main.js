47952e893866a7c4fad844f42d0cf1c0638c0c55
Quentin FONCK <quentin.fonck@internet.lu>
2015-10-09 16:35:49 +0200

    Menu mobile fait sur l'accueil internet

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -1,1 +1,49 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+// ON PAGE READY
+$(function () {
+    OpenMenu.init();
+});
+
+
+
+
+// Permet d'ouvrir le menu de droite sur mobile
+var OpenMenu = (function () {
+    //Cache DOM
+    var $mobileButton = $('.mobile-button-white');
+    var $contentSite = $('#siteWrap');
+    var $mobileMenu = $('.mobile-side-menu>.menu');
+    var $document = $('html');
+
+    var init = function () {
+        console.log('hola chicka');
+        _bindEvents();
+    };
+    var _bindEvents = function () {
+        $mobileButton.on('click', _toggleSideMenu.bind(this));
+        //$document.on('click', _closeSideMenuOnAway.bind(this));
+        $document.on('resize', _closeSideMenu.bind(this));
+    };
+    
+    var _toggleSideMenu = function(){
+        $contentSite.toggleClass('open');
+    };
+    var _openSideMenu = function (event) {
+        if (!$contentSite.hasClass('open')) {
+            $contentSite.addClass('open');
+        }
+    };
+    var _closeSideMenuOnAway = function (event) {
+        if (!$(event.target).closest($mobileMenu, $mobileButton).length) {
+            _closeSideMenu();
+        }
+    };
+    var _closeSideMenu = function (event) {
+        if ($contentSite.hasClass('open')) {
+            $contentSite.removeClass('open');
+        }
+    };
+    return{
+        init: init
+    };
+})();

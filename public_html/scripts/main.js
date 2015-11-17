6ffef3d3e943b6bf7566e0c764b902f0c26c149a
Quentin FONCK <quentin.fonck@internet.lu>
2015-11-17 14:46:30 +0100

    Modification du script pour le formulaire

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -51,6 +51,12 @@ $(function () {
     }
 });
 
+//test si l'adresse email est eligible
+function testEmail(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+}
+
 //size of viewport
 function viewport() {
     var e = window, a = 'inner';
@@ -137,9 +143,9 @@ var EquipementFilter = (function () {
             $searchDeskMenuItem.removeClass('is-active');
             element.addClass('is-active');
         }
-        if (element.attr('data-cat') === 'tous'){
+        if (element.attr('data-cat') === 'tous') {
             $categories.removeClass('is-hidden');
-        }else{
+        } else {
             $('#' + element.attr('data-cat') + '').removeClass('is-hidden');
             $('#' + element.attr('data-cat') + '').show().flickity('resize');
         }
